eee7bc6429c973aa2116f3639def7523c9e19f0e
Tyler Ryan <tjryan2424@gmail.com>
2018-12-13 17:03:29 -0800

    Add higher order components for Authentication and Authorization

---
+++ src/components/Session/withAuthentication.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import AuthUserContext from './context'
+import { withFirebase } from '../Firebase'
+
+const withAuthentication = Component => {
+  class WithAuthentication extends React.Component {
+    constructor (props) {
+      super(props)
+
+      this.state = {
+        authUser: null
+      }
+    }
+
+    componentDidMount () {
+      this.unsubscribeListener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+        authUser
+          ? this.setState({ authUser })
+          : this.setState({ authUser: null })
+      })
+    }
+
+    componentWillUnmount () {
+      this.unsubscribeListener()
+    }
+
+    render () {
+      return (
+        <AuthUserContext.Provider value={this.state.authUser}>
+          <Component {...this.props} />
+        </AuthUserContext.Provider>
+      )
+    }
+  }
+  return withFirebase(WithAuthentication)
+}
+
+export default withAuthentication
---
+++ src/components/Session/withAuthorization.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import { compose } from 'recompose'
+
+import AuthUserContext from './context'
+import { withFirebase } from '../Firebase'
+import * as ROUTES from '../../constants/routes'
+
+const withAuthorization = condition => Component => {
+  class WithAuthorization extends React.Component {
+    componentDidMount () {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        authUser => {
+          // If the authorization condition fails,
+          // the user will be redirected to the sign in page.
+          if (!condition(authUser)) {
+            this.props.history.push(ROUTES.SIGN_IN)
+          }
+        }
+      )
+    }
+
+    componentWillUnmount () {
+      this.listener()
+    }
+
+    render () {
+      return (
+        <AuthUserContext.Consumer>
+          {authUser =>
+            condition(authUser) ? <Component {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
+      )
+    }
+  }
+
+  return compose(
+    withRouter,
+    withFirebase
+  )(WithAuthorization)
+}
+
+export default withAuthorization
